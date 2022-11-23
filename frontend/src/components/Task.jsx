1a710bac5532ba3a6ed6b0b9e470c59e53539c38
alon1302 <66137565+alon1302@users.noreply.github.com>
2022-11-23 19:33:43 +0200

    handle delete task

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Task = ({ task }) => {
-  return <h1>{task}</h1>;
+const Task = ({ task, handleDelete }) => {
+  return (
+    <div className="taskItem">
+      <span>{task.content}</span>
+      <button onClick={() => handleDelete(task.id)}>delete</button>
+    </div>
+  );
 };
 
 export default Task;
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -2,8 +2,21 @@ import React, { useState } from "react";
 import Task from "./Task";
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState(["alon", "roy"]);
-  return tasks.map((task) => <Task task={task} />);
+  const [tasks, setTasks] = useState([
+    { content: "alon", id: 1 },
+    { content: "roy", id: 2 },
+  ]);
+
+  const handleDelete = (id) => {
+    console.log("click");
+    let newtasks = [...tasks];
+    newtasks = newtasks.filter((task) => task.id != id);
+    setTasks(newtasks);
+  };
+
+  return tasks.map((task) => (
+    <Task task={task} handleDelete={handleDelete} key={task.id} />
+  ));
 };
 
 export default TaskList;
