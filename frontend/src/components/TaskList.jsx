711d5e6bc371afda651982f8ba24e1a1c738c1f0
alon1302 <66137565+alon1302@users.noreply.github.com>
2022-12-05 10:58:20 +0200

    styling the list of tasks

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import TaskItem from "./TaskItem";
 
+import "../style/TaskList.css";
+
 const TaskList = (props) => {
   return props.tasks.map((task) => (
-    <TaskItem
-      task={task}
-      handleDelete={props.handleDelete}
-      handleChecked={props.handleChecked}
-      key={task.id}
-    />
+    <div className="task-list">
+      <TaskItem
+        task={task}
+        handleDelete={props.handleDelete}
+        handleChecked={props.handleChecked}
+        key={task.id}
+      />
+    </div>
   ));
 };
 
